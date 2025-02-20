64fc85eac4958b449e788694b5d83f143c8dd8b8
Your NameCristiana <papercri@gmail.com>
2025-02-20 13:53:54 +0100

    Nivel2-Ejercicio 5 . Sin maquetar

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,22 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+//Bootstrap Form Validator
+
+(function () {
+    'use strict'
+  
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()

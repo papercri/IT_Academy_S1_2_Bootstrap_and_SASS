39c595628411707ddf63dedd7a05a96677926890
Your NameCristiana <papercri@gmail.com>
2025-02-21 12:32:16 +0100

    Exercici 6 (N3) Canvi estat actius

--- src/js/main.js
+++ src/js/main.js
@@ -5,37 +5,35 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 //Bootstrap Form Validator
-
-(function () {
-    'use strict'
-  
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  (function () {
+      'use strict'
+    
+      var forms = document.querySelectorAll('.needs-validation')
+    
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+    
+            form.classList.add('was-validated')
+          }, false)
+        })
+    })()
 
   //Mobile menu
+    document.addEventListener("DOMContentLoaded", function () {
+      const menuToggle = document.getElementById("mobileMenuToggle");
+      const mobileMenu = document.getElementById("mobileMenu");
+      const closeMenu = document.getElementById("closeMenu");
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("mobileMenuToggle");
-    const mobileMenu = document.getElementById("mobileMenu");
-    const closeMenu = document.getElementById("closeMenu");
+      menuToggle.addEventListener("click", function () {
+          mobileMenu.classList.add("active");
+      });
 
-    menuToggle.addEventListener("click", function () {
-        mobileMenu.classList.add("active");
-    });
-
-    closeMenu.addEventListener("click", function () {
-        mobileMenu.classList.remove("active");
-    });
-});
+      closeMenu.addEventListener("click", function () {
+          mobileMenu.classList.remove("active");
+      });
+  });
